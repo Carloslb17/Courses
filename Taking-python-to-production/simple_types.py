f3d1e64a94956adb675244e70680c668c0d013b2
Carlos Lopez <132601006+Carloslb17@users.noreply.github.com>
2025-03-09 16:18:08 +0000

    Nested Types and annotations

--- Taking-python-to-production/simple_types.py
+++ Taking-python-to-production/simple_types.py
@@ -16,7 +16,7 @@ def many_simple_types(
 
 
 
-from typing import Any, List, Tuple, Dict, Set
+from typing import Any, List, Tuple, Dict, Set, Union, Type, Optional, TypedDict, NamedTuple
 
 nums: List[int] = []
 vector_xy: Tuple[int] = (1,2)
@@ -29,3 +29,88 @@ fruits: Set[str] = {"apple", "kiwi"}
 
 
 class Animal:
+    pass
+
+
+misc_values: List[Union[int, float, str, Type]] = [1, 1.0, "hi", object]
+
+# Using optional for None situations. 
+x: Union[int, None] = None
+x: Optional[int] = None
+# Modern sintax:
+x: str | None = None
+
+
+# - Nested types
+
+student: Dict[str, Union[str, int]] = {
+                "Bob": 1, 
+}
+
+## Class for anotated Dictionaries.
+
+class Student(TypedDict):
+    name: str
+    age: int 
+
+## Improves readibility and enforces a way to structure predefined diciotnaries. 
+student: Student = {
+                "name": "Bob", 
+                "age":1,
+}
+
+## Also can be used to create dictionaries, and adds autocompletition for dicts
+Student(name="Bob", age="4")
+
+## Complex types.
+
+from collections import namedtuple
+
+
+point_2d = (1, 2)
+
+Point = namedtuple("Point", ["x", "y"])
+point_2d = Point(1, 2)
+point_2d.x
+point_2d.y
+
+class Point(NamedTuple):
+    x: int
+    y: int
+    
+    
+point_2d = Point(1, 2)
+point_2d.x
+point_2d.y
+
+## Recursive types.
+###
+
+
+class Student(TypedDict):
+    name: str
+    age: int 
+    friends: List["Student"] # Double quoting the class we get the autocompletition. 
+    
+    
+student: Student = {
+                "name": "Bob", 
+                "age":1,
+                "friends": {
+                    "name": "Bob", 
+                    "age":1,
+                    "friends":[]                    
+                    
+                }
+                    
+}
+
+### In modern versions of Python "" don't need to be included. 
+from __future__ import annotations
+
+class Student(TypedDict):
+    name: str
+    age: int 
+    friends: List[Student] 
+    
+
