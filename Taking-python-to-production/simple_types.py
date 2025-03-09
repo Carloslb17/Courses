5524ff92bb0425567474bd3abb84011a97c2189e
Carlos Lopez <132601006+Carloslb17@users.noreply.github.com>
2025-03-09 17:53:58 +0000

    Generic Types

--- Taking-python-to-production/simple_types.py
+++ Taking-python-to-production/simple_types.py
@@ -113,4 +113,65 @@ class Student(TypedDict):
     age: int 
     friends: List[Student] 
     
+    
+    
+    
+TStudentArgsDict = Dict[str, int, List[Student]]
 
+TAddableEntity = Union[int, float, str, list]
+
+def add(a:TAddableEntity , b: TAddableEntity) -> TAddableEntity:
+    return a + b
+
+### Generic Types
+
+from typing import TypeVar, Generic
+
+### Place holder type
+TAddableEntity = TypeVar["TAddableEntity", int, float, str, list]
+TException = TypeVar("Exception", bound=Exception)
+
+### 
+def add(a:TAddableEntity , b: TAddableEntity) -> TAddableEntity:
+    return a + b
+
+def raise_exception(err: TException):
+    raise err
+
+# Same as above but simplified. 
+def raise_exception(err: Exception):
+    raise err
+
+raise_exception(ValueError("Error"))
+
+
+## Advanced definition of generics.
+### Inheritance tree, avoid o high maintance
+### Use Generic from typing for complex structures
+
+from dataclasses import dataclass
+
+TFriend1 = TypeVar("TFriend")
+TFriend2 = TypeVar("TFriend")
+
+@dataclass
+class Student(Generic[TFriend1]):
+    name: str
+    age: int 
+    friends_1: List[TFriend1] 
+    friends_2: List[TFriend2] 
+    
+    
+    
+class Animal:
+    ...
+
+
+## The following method  ensures that python know all the time what type is the objects we passed to the 
+## the class
+student_1 : Student[Animal, int] = Student(
+            name="Bob",
+            age=16,
+            friends_1=[Animal()],
+            friends_2=[1],
+)
